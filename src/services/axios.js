47cfd992574ef7a02392a25744bd480abf9564d9
Tigran Yeghyan <171566309+arxitekt0r@users.noreply.github.com>
2025-05-11 21:34:47 +0400

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,13 +1,10 @@
-import axios from 'axios'
-import qs from 'qs'
+import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://api.uatsystems.dev',
   headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
-  transformRequest: [(data) => qs.stringify(data)],
-  withCredentials: false, // Optional: true if you want to send cookies
-})
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+});
 
-export default instance
+export default instance;
